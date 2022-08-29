1e7a3282e8bc6cca55d5d8a478d989b1c3a910b5
adamgordon241 <102659680+adamgordon241@users.noreply.github.com>
2022-08-29 23:34:14 +0200

    Users email visible + minor changes
    Users email is now visible in the accounts page, minor changes and corrections
    
    Co-Authored-By: Jaden0902 <102761782+Jaden0902@users.noreply.github.com>
    Co-Authored-By: yasirad <101118594+yasirad@users.noreply.github.com>
    Co-Authored-By: lerushisonfire1 <103134377+lerushisonfire1@users.noreply.github.com>
    Co-Authored-By: wasimrahiman <102312805+wasimrahiman@users.noreply.github.com>

--- shopping-list-wits/src/contexts/AuthContext.js
+++ shopping-list-wits/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import {createUserWithEmailAndPassword} from 'firebase/auth'
 import {signInWithEmailAndPassword} from 'firebase/auth'
 import {signOut} from 'firebase/auth'
@@ -11,16 +11,32 @@ const UserContext=createContext();
 //takes in children
 export const AuthContextProvider=({children})=>{
 
+    //need state for access
+    //use state takes in obkect->{}
+    const [user,setUser]=useState({});
+
     //create a new user function
     const createUser=(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password);
 
     };
 
+    /*use onAuthStateChange-> put inside useEffect as only want it to run once when 
+      component mounts*/
+
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,(currentUser)=>{
+            console.log(currentUser);
+            setUser(currentUser);
+        });
+        return ()=>{
+            unsubscribe();
+        }
+    },[]);
 
     return (
-        //this is where we export all our values/functions
-        <UserContext.Provider value={{createUser}}>
+        //this is where we export all our values/functions-imported via UserAuth and accessed as objects
+        <UserContext.Provider value={{createUser, user}}>
             {children}
         </UserContext.Provider>
     )
