f647d1bbf17f46bd65b036edef8a8e38e41c59e6
lerushisonfire1 <2305631@students.wits.ac.za>
2022-09-13 19:27:00 +0200

    Mobile App
    Co-Authored-By: wasimrahiman <102312805+wasimrahiman@users.noreply.github.com>
    Co-Authored-By: yasirad <101118594+yasirad@users.noreply.github.com>
    Co-Authored-By: Jaden0902 <102761782+Jaden0902@users.noreply.github.com>
    Co-Authored-By: adamgordon241 <102659680+adamgordon241@users.noreply.github.com>

---
+++ MobileShoppingList/AuthContext.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signOut } from 'firebase/auth'
+//checks if logged in therefore have access to protected routes
+
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebase';
+import { sendPasswordResetEmail } from "firebase/auth";
+
+
+const UserContext = createContext();
+
+//takes in children
+export const AuthContextProvider = ({ children }) => {
+
+    //need state for access
+    //use state takes in obkect->{}
+    const [user, setUser] = useState({});
+
+    //create a new user function
+    const createUser = (email, password) => {
+        //firebase function for creating user
+        return createUserWithEmailAndPassword(auth, email, password);
+
+    };
+
+    //login function
+    const signIn = (email, password) => {
+        //firebase function for loggin in
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    //logout function
+    const logout = () => {
+        //firbase function for logging out
+        return signOut(auth);
+    }
+
+
+    //forgotpasswordFunction
+    const forgotPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    /*use onAuthStateChange-> put inside useEffect as only want it to run once when
+    /*use onAuthStateChange-> put inside useEffect as only want it to run once when
+      component mounts*/
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log(currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    return (
+        //this is where we export all our values/functions-imported via UserAuth and accessed as objects
+        <UserContext.Provider value={{createUser, user, logout, signIn, forgotPassword}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+
+
+export const UserAuth = () => {
+        //this is what makes context avaliable
+        return useContext(UserContext)
+    }
---
+++ MobileShoppingList/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDAteV2w78EM0zrd13Q-9ycPBjyxiuY51I",
+    authDomain: "shopping-list-wits.firebaseapp.com",
+    projectId: "shopping-list-wits",
+    storageBucket: "shopping-list-wits.appspot.com",
+    messagingSenderId: "186447090602",
+    appId: "1:186447090602:web:0970f63956c1140bd991d3",
+    measurementId: "G-1D1Z52WMBB"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
