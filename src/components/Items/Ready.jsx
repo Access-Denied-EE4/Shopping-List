1c3d6893515c8cb716f7b5a502db74ea0b11c391
adamgordon241 <102659680+adamgordon241@users.noreply.github.com>
2022-09-25 13:05:49 +0200

    Meat and Ready Made categories img works
    Co-Authored-By: yasirad <101118594+yasirad@users.noreply.github.com>
    Co-Authored-By: Jaden0902 <102761782+Jaden0902@users.noreply.github.com>
    Co-Authored-By: lerushisonfire1 <103134377+lerushisonfire1@users.noreply.github.com>
    Co-Authored-By: wasimrahiman <102312805+wasimrahiman@users.noreply.github.com>

--- src/components/Items/Ready.jsx
+++ src/components/Items/Ready.jsx
@@ -3,8 +3,9 @@ import {Avatar, Card, Container, ImageList, ImageListItem, ImageListItemBar, Too
 import AddCircleIcon from '@mui/icons-material/AddCircleOutline';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import {db} from "../../firebase";
+import {db, storage} from "../../firebase";
 import {collection, getDocs} from 'firebase/firestore';
+import {ref, getDownloadURL} from 'firebase/storage';
 import ctc from "../../images/CTCC.jpg"
 
 const Ready = () => {
@@ -18,6 +19,8 @@ const Ready = () => {
   //pass in db variable from fireabse file, collection name in DB
   //collection is a firebase function 
   const readyItemsCollectionRef=collection(db, "ready_items");
+    //state for image, defualt is null
+    const [url, setUrl]=useState([]);
 
   //need to display all the ready items immedailty when the page is loaded without having to click on a button
   //useEffect hook -> function to be called that allows info to be loaded as soon as the page is loaded
@@ -45,7 +48,27 @@ const Ready = () => {
 
     //call async function 
     getReadyItems();
-  },[]);
+  },[readyItems]);
+
+  //use effect handling thr retrivel of imags from the database
+  useEffect(()=>{
+    //create an async functiopn so we can use the await key word
+    const getImgUrl=async()=>{
+      //image array wil store an object made from the items name and a url to the image 
+      const imageArray=[];
+      for(let i=0; i<readyItems.length; ++i)
+      {
+          //get url for the image of the relevant itme
+          const imgUrl=await getDownloadURL(ref(storage,readyItems[i].img_url));
+          //create objecr 
+          imageArray.push({name: `${readyItems[i].name}`, url: `${imgUrl}`});
+      }
+      //set the url state to the image array
+      setUrl(imageArray);
+    }
+    getImgUrl();
+
+  },[url]);
 
   return (
     <>
@@ -58,10 +81,24 @@ const Ready = () => {
               gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))!important',
           }}>
               {readyItems.map(item=>{
+                //stores image url to be used
+                let img;
+                //need in if statemnt to handle useffect has not run yet
+                if(url.length!=0)
+                {
+                  //if our url has been populated then use the image
+                  let img_url=url.find(img=>img.name===item.name);
+                  img=img_url.url;
+                }
+                else
+                {
+                  //if it has not then use our logo and on the next render it will change 
+                  img=ctc;
+                }
                 return(
                   <Card key={item.id}>
                     <ImageListItem sx={{height: '100% !important'}}>
-                        <img src={ctc} style={{cursor:'pointer'}}></img>
+                        <img src={img} style={{cursor:'pointer'}}></img>
                         <ImageListItemBar 
                           title={item.name}
                           actionIcon={
