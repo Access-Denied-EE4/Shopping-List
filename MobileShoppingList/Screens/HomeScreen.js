2b51703b3c6948197076c4627d694bfde8b69fe0
Jaden0902 <2169997@students.wits.ac.za>
2022-09-26 14:20:09 +0200

    updated homescreen
    Co-Authored-By: adamgordon241 <102659680+adamgordon241@users.noreply.github.com>
    Co-Authored-By: lerushisonfire1 <103134377+lerushisonfire1@users.noreply.github.com>
    Co-Authored-By: wasimrahiman <102312805+wasimrahiman@users.noreply.github.com>
    Co-Authored-By: yasirad <101118594+yasirad@users.noreply.github.com>

--- MobileShoppingList/Screens/HomeScreen.js
+++ MobileShoppingList/Screens/HomeScreen.js
@@ -1,17 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View,ImageBackground } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View,ScrollView,Image, Button, Pressable, ImageBackground} from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
 import { useNavigation } from '@react-navigation/core'
+import Dairy from '../images/dairy.webp';
+import Chocolate from '../images/chocolate.jpeg' 
+import Drinks from '../images/drinks.jpeg'
+import Meat from '../images/meat.jpeg'
+import ReadyMade from '../images/readyMade.webp'
+import Toiletries from '../images/Toiletries.jpeg'
+import Veggies from '../images/vegetables.jpeg'
+
 
 const HomeScreen = () => {
 
   const navigation = useNavigation();
+
+  const logoPic = require("../images/ClickToCart.jpg");
   
 
   const handleLogOut = () => {
 
     auth.signOut().then( () => {
-      navigation.replace("Login");
+      navigation.navigate("Login");
     })
 
     .catch(error => alert(error.message))
@@ -24,21 +34,42 @@ const HomeScreen = () => {
 
   return (
 
-    
-    <View style = { styles.container}>
-
-    
-    <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress = {handleLogOut}
-        style = {styles.button}
-      >
-
-        <Text style = {styles.buttonText}>Log Out</Text>
-      </TouchableOpacity>
-    
+  
       
+      
+    <View>
+
+     
+  
+
+        <ScrollView>
+       
+
+        <Pressable
+          onPress = {handleLogOut}
+          style = {[styles.button,styles.container]}
+          hitSlop={{top: 50, bottom: 150, left: 50}}
+         
+        >
+
+          <Text style={styles.buttonText}>Logout</Text>
+        </Pressable>
+     
+       
+        <Image style ={[styles.image]} source = {Dairy}></Image>
+        <Image style ={[styles.image]} source = {Chocolate}></Image>
+        <Image style ={[styles.image]} source = {Drinks}></Image>
+        <Image style ={[styles.image]} source = {Meat}></Image>
+        <Image style ={[styles.image]} source = {ReadyMade}></Image>
+        <Image style ={[styles.image]} source = {Toiletries}></Image>
+        <Image style ={[styles.image]} source = {Veggies}></Image>
+    
+     
+        </ScrollView>
+
     </View>
+    
+   
   )
 }
 
@@ -48,29 +79,42 @@ const styles = StyleSheet.create({
 
   container: {
 
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: 'absolute',
+    right: 5,
+    top: 2,
 
 
   },
   button: {
 
-    backgroundColor:'#0782F9',
-    width:'100%',
-    padding:15,
+    backgroundColor:'#2A2C41',
+    width:'20%',
+    padding:5,
     borderRadius:10,
     alignItems:'center',
-    marginTop: 40,
+    marginTop: 10,
+   
 
   },
   buttonText: {
 
-    color: 'black',
+    color: 'white',
     fontWeight: '700',
     fontSize:16,
 
   },
+  image: {
+    flex: 1,
+    width: 350,
+    height: 350,
+    resizeMode: 'contain' ,
+    left: 20,
+   
+  },
+ 
+  
+ 
+
  
 
 })
